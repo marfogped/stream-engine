92bd219f158b2f2a530f1ad37a5b90821309fc43
mrtfog <fogliaccomartin3@gmail.com>
2024-07-20 18:50:51 -0300

    add: Chackra UI configurations

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 import "@fontsource-variable/dm-sans";
 import "./index.css";
 
@@ -17,5 +18,7 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={router} />
+  <ChakraProvider>
+    <RouterProvider router={router} />
+  </ChakraProvider>
 );
