5b6990f9f754e28431da4cef61174c2dfd4847ea
mrtfog <fogliaccomartin3@gmail.com>
2024-07-20 18:24:13 -0300

    add: customers logos

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,9 +1,31 @@
 import StreamEngineLogo from "../assets/img/stream-engine-logo.svg";
 import DefenseBackground from "../assets/img/defense-background.jpeg";
 import AdvisoryBackground from "../assets/img/advisory-background.png";
+import Comcast from "../assets/img/comcast.png";
+import Ea from "../assets/img/ea.png";
+import Damage from "../assets/img/damage.png";
+import Endeavor from "../assets/img/endeavor.png";
+import Havas from "../assets/img/havas.png";
+import Marketcast from "../assets/img/marketcast.png";
+import Navigate from "../assets/img/navigate.png";
+import Pga from "../assets/img/pga.png";
+import Teamenvy from "../assets/img/teamenvy.png";
+import Usta from "../assets/img/usta.png";
+import Uta from "../assets/img/uta.png";
 
 export default {
   StreamEngineLogo,
   DefenseBackground,
   AdvisoryBackground,
+  Comcast,
+  Ea,
+  Damage,
+  Endeavor,
+  Havas,
+  Marketcast,
+  Navigate,
+  Pga,
+  Teamenvy,
+  Usta,
+  Uta,
 };
