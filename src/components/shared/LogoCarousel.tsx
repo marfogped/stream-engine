0a8d0d1d61bd15e14600bf75d00e546d4f829399
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-07-22 12:43:26 -0300

    fix: Logos sizes

--- src/components/shared/LogoCarousel.tsx
+++ src/components/shared/LogoCarousel.tsx
@@ -10,7 +10,7 @@ interface LogoCarouselProps {
 const LogoCarousel: React.FC<LogoCarouselProps> = ({ logos }) => {
   return (
     <div className="py-8 w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
-      <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+      <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-28 [&_img]:max-h-14 animate-infinite-scroll">
         {logos.map((logo, index) => (
           <li key={index}>
             <img src={logo.image} alt={logo.alt} />
@@ -18,7 +18,7 @@ const LogoCarousel: React.FC<LogoCarouselProps> = ({ logos }) => {
         ))}
       </ul>
       <ul
-        className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll"
+        className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-28 [&_img]:max-h-14 animate-infinite-scroll"
         aria-hidden="true"
       >
         {logos.map((logo, index) => (
