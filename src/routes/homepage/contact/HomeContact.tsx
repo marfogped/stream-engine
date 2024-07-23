d2802f8e17c47b9e2ba9e10a4d5b943ec3600fb0
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-07-23 16:37:21 -0300

    feat: Map componente mounted at contact section

--- src/routes/homepage/contact/HomeContact.tsx
+++ src/routes/homepage/contact/HomeContact.tsx
@@ -5,6 +5,7 @@ import { Typography } from "../../../components/ui";
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 import { constants } from "../../../constants";
 import { ContainerAnimation } from "../../../components/ui";
+import { MapComponent } from "../../../components/shared";
 
 const HomeContact: React.FC = () => {
   const sendMessage = () => {
@@ -12,137 +13,148 @@ const HomeContact: React.FC = () => {
   };
 
   return (
-    <section className="page-width flex xs:flex-col sm:flex-col lg:flex-row justify-start lg:mt-[80px] ">
-      <div className="flex flex-col w-full mt-[80px] lg:mt-[0] justify-between">
-        <ContainerAnimation className="flex flex-col">
-          <Typography
-            as="h2"
-            variant="xl"
-            color="black"
-            className="leading-tight"
-          >
-            Contact Us
-          </Typography>
-          <Typography as="p" variant="md" className="text-gray-700">
-            Get in touch with us to learn more about our innovative solutions,
-            ask questions, or receive support.
-          </Typography>
-        </ContainerAnimation>
-
-        <ContainerAnimation className="lg:flex flex-col gap-[5px] hidden">
-          <Typography as="p" variant="md" className="text-gray-700">
-            <a
-              className="flex items-center gap-2 underline w-max"
-              href="mailto:info@streamengine.com"
+    <>
+      <section className="page-width flex xs:flex-col sm:flex-col lg:flex-row justify-start lg:mt-[80px]">
+        <div className="flex flex-col w-full mt-[80px] lg:mt-[0] justify-between">
+          <ContainerAnimation className="flex flex-col">
+            <Typography
+              as="h2"
+              variant="xl"
+              color="black"
+              className="leading-tight"
             >
-              info@streamengine.com{" "}
-              <ArrowTopRightOnSquareIcon className="w-6 h-6" />{" "}
-            </a>
-          </Typography>
+              Contact Us
+            </Typography>
+            <Typography as="p" variant="md" className="text-gray-700">
+              Get in touch with us to learn more about our innovative solutions,
+              ask questions, or receive support.
+            </Typography>
+          </ContainerAnimation>
 
-          <div className="flex items-center gap-[5px]">
-            {constants.socials.map((social) => (
+          <ContainerAnimation className="lg:flex flex-col gap-[5px] hidden">
+            <Typography as="p" variant="md" className="text-gray-700">
               <a
-                aria-label={`${social.alt}`}
-                key={social.id}
-                href={social.url}
-                target="_blank"
-                className="cursor-pointer transition-all hover:scale-125"
+                className="flex items-center gap-2 underline w-max"
+                href="mailto:info@streamengine.com"
               >
-                {social.name.toLowerCase() === "whatsapp" && (
-                  <social.icon className="text-gray-700" />
-                )}
-                {social.name.toLowerCase() === "linkedin" && (
-                  <social.icon className="text-gray-700" />
-                )}
-                {social.name.toLowerCase() === "instagram" && (
-                  <social.icon className="text-gray-700" />
-                )}
-                {social.name.toLowerCase() === "x" && (
-                  <social.icon className="text-gray-700" />
-                )}
-                {social.name.toLowerCase() === "facebook" && (
-                  <social.icon className="text-gray-700" />
-                )}
+                info@streamengine.com{" "}
+                <ArrowTopRightOnSquareIcon className="w-6 h-6" />{" "}
               </a>
-            ))}
-          </div>
+            </Typography>
+
+            <div className="flex items-center gap-[5px]">
+              {constants.socials.map((social) => (
+                <a
+                  aria-label={`${social.alt}`}
+                  key={social.id}
+                  href={social.url}
+                  target="_blank"
+                  className="cursor-pointer transition-all hover:scale-125"
+                >
+                  {social.name.toLowerCase() === "whatsapp" && (
+                    <social.icon className="text-gray-700" />
+                  )}
+                  {social.name.toLowerCase() === "linkedin" && (
+                    <social.icon className="text-gray-700" />
+                  )}
+                  {social.name.toLowerCase() === "instagram" && (
+                    <social.icon className="text-gray-700" />
+                  )}
+                  {social.name.toLowerCase() === "x" && (
+                    <social.icon className="text-gray-700" />
+                  )}
+                  {social.name.toLowerCase() === "facebook" && (
+                    <social.icon className="text-gray-700" />
+                  )}
+                </a>
+              ))}
+            </div>
+          </ContainerAnimation>
+        </div>
+
+        <ContainerAnimation className="w-full h-full">
+          <form
+            action="POST"
+            onSubmit={sendMessage}
+            className="flex flex-col items-center gap-6 w-full mt-[40px] lg:mt-[0] "
+          >
+            <div className="grid gap-6 sm:grid-cols-2 w-full">
+              <div className="relative z-0">
+                <input
+                  type="text"
+                  name="firstName"
+                  className="peer form-input"
+                  placeholder=" "
+                  aria-label="First Name"
+                />
+                <label className="form-label">First name</label>
+              </div>
+
+              <div className="relative z-0">
+                <input
+                  type="text"
+                  name="lastName"
+                  className="peer form-input"
+                  placeholder=" "
+                  aria-label="Last Name"
+                />
+                <label className="form-label">Last name</label>
+              </div>
+
+              <div className="relative z-0">
+                <input
+                  type="text"
+                  name="email"
+                  className="peer form-input"
+                  placeholder=" "
+                  aria-label="Email"
+                />
+                <label className="form-label">Email</label>
+              </div>
+
+              <div className="relative z-0">
+                <input
+                  type="text"
+                  name="phone"
+                  className="peer form-input"
+                  placeholder=" "
+                  aria-label="Phone"
+                />
+                <label className="form-label">Phone</label>
+              </div>
+
+              <div className="relative z-0 col-span-2">
+                <textarea
+                  name="message"
+                  className="peer resize-none h-32 form-input"
+                  placeholder=" "
+                  aria-label="Your Message"
+                ></textarea>
+                <label className="form-label">Your message</label>
+              </div>
+
+              <Button
+                className="mt-5 col-span-2 flex items-center gap-2"
+                bg="#F8F603"
+                _hover={{ bg: "#F8F603" }}
+                type="submit"
+              >
+                Send Message <PaperAirplaneIcon className="h-7 w-7" />
+              </Button>
+            </div>
+          </form>
         </ContainerAnimation>
-      </div>
-
-      <ContainerAnimation className="w-full h-full">
-        <form
-          action="POST"
-          onSubmit={sendMessage}
-          className="flex flex-col items-center gap-6 w-full mt-[40px] lg:mt-[0] "
-        >
-          <div className="grid gap-6 sm:grid-cols-2 w-full">
-            <div className="relative z-0">
-              <input
-                type="text"
-                name="firstName"
-                className="peer form-input"
-                placeholder=" "
-                aria-label="First Name"
-              />
-              <label className="form-label">First name</label>
-            </div>
-
-            <div className="relative z-0">
-              <input
-                type="text"
-                name="lastName"
-                className="peer form-input"
-                placeholder=" "
-                aria-label="Last Name"
-              />
-              <label className="form-label">Last name</label>
-            </div>
-
-            <div className="relative z-0">
-              <input
-                type="text"
-                name="email"
-                className="peer form-input"
-                placeholder=" "
-                aria-label="Email"
-              />
-              <label className="form-label">Email</label>
-            </div>
-
-            <div className="relative z-0">
-              <input
-                type="text"
-                name="phone"
-                className="peer form-input"
-                placeholder=" "
-                aria-label="Phone"
-              />
-              <label className="form-label">Phone</label>
-            </div>
-
-            <div className="relative z-0 col-span-2">
-              <textarea
-                name="message"
-                className="peer resize-none h-32 form-input"
-                placeholder=" "
-                aria-label="Your Message"
-              ></textarea>
-              <label className="form-label">Your message</label>
-            </div>
-
-            <Button
-              className="mt-5 col-span-2 flex items-center gap-2"
-              bg="#F8F603"
-              _hover={{ bg: "#F8F603" }}
-              type="submit"
-            >
-              Send Message <PaperAirplaneIcon className="h-7 w-7" />
-            </Button>
-          </div>
-        </form>
-      </ContainerAnimation>
-    </section>
+      </section>
+      <section className="page-width lg:mt-[40px]">
+        <span className="sr-only">Stream Engine Map Location</span>
+        <ContainerAnimation className="h-max w-full">
+          <MapComponent
+            position={[34.0522, -118.2437]}
+            className="xl:h-[400px] h-[200px] w-full"
+          />
+        </ContainerAnimation>
+      </section>
+    </>
   );
 };
 
