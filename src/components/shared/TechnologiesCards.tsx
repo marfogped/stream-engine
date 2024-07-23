6aed5a7acf688774b0ad1fd80fdda494e4b3644b
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-07-23 12:55:27 -0300

    fix: Technology Type moved to types.ts file, and card background changed to a video

--- src/components/shared/TechnologiesCards.tsx
+++ src/components/shared/TechnologiesCards.tsx
@@ -1,15 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Typography } from "../ui";
-
-interface TechnologyProps {
-  id: string;
-  title: string;
-  icon: React.ReactNode;
-  image: string;
-  cols: number;
-  rows: number;
-  description: string;
-}
+import { TechnologyProps } from "../../lib/types";
 
 interface CardProps {
   technology: TechnologyProps;
@@ -18,34 +9,53 @@ interface CardProps {
 }
 
 const TechnologiesCards: React.FC<CardProps> = ({ technology, className }) => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  const handleMouseEnter = () => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+    }
+  };
+
   return (
     <article
       style={{
-        background: `url(${technology.image})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
         gridColumn: `span ${technology.cols}`,
         gridRow: `span ${technology.rows}`,
       }}
-      className={`${className} flex flex-col justify-end items-start xs:h-[284px] sm:h-[284px] lg:h-full rounded-[10px] p-5 relative overflow-hidden`}
+      className={`${className} flex flex-col justify-end items-start xs:h-[284px] sm:h-[284px] lg:h-full rounded-[10px] p-5 relative overflow-hidden group`}
     >
-      <div className="absolute top-0 left-0 w-full h-full bg-black/60 z-[1]" />
-      <div className="z-[2] flex flex-col">
-        <div className="flex flex-col gap-2">
-          <div className="">
-            {technology.icon}
-            <Typography variant="lg" color="white" className="leading-none">
-              {technology.title}
-            </Typography>
-          </div>
-          <Typography
-            variant="sm"
-            className="leading-tight text-gray-300 overflow-hidden"
-          >
-            {technology.description}
-          </Typography>
-        </div>
+      <video
+        ref={videoRef}
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        width="640"
+        height="480"
+        loop
+        muted
+        className="z-[1] w-full h-full object-cover object-center scale-[135%] absolute top-0 left-0"
+      >
+        <source src={technology.video} type="video/mp4" />
+      </video>
+      <div className="absolute top-0 left-0 w-full h-full bg-black/60 z-[1] pointer-events-none" />
+      <div className="z-[2] flex flex-col gap-2 pointer-events-none">
+        {technology.icon}
+        <Typography variant="lg" color="white" className="leading-none">
+          {technology.title}
+        </Typography>
+
+        <Typography
+          variant="sm"
+          className="leading-tight text-gray-300 overflow-hidden lg:hidden lg:group-hover:flex origin-bottom"
+        >
+          {technology.description}
+        </Typography>
       </div>
     </article>
   );
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -10,3 +10,13 @@ export interface ServicesProps {
   description: string;
   backgroundImage: string;
 }
+
+export interface TechnologyProps {
+  id: string;
+  title: string;
+  icon: React.ReactNode;
+  video: string;
+  cols: number;
+  rows: number;
+  description: string;
+}
