ceeb5c323122c3b9f7943175cc8a33a543ba54a1
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-07-23 12:48:42 -0300

    add: videos for technologies cards

--- src/constants/videos.ts
+++ src/constants/videos.ts
@@ -1,7 +1,16 @@
 import DefenseVideo from "../assets/videos/defense-video.mp4";
 import AdvertisingVideo from "../assets/videos/advertising-video.mp4";
 
+import SocialTechBackground from "../assets/videos/social-technology.webm";
+import TranslationsTechBackground from "../assets/videos/translations-technology.webm";
+import VisionTechBackground from "../assets/videos/vision-technology.webm";
+import VoiceTechBackground from "../assets/videos/voice-technology.webm";
+
 export default {
   DefenseVideo,
   AdvertisingVideo,
+  SocialTechBackground,
+  TranslationsTechBackground,
+  VisionTechBackground,
+  VoiceTechBackground,
 };
