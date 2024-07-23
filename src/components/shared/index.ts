97ad067b3cd51e4fec4cf2ace5e9761796564efd
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-07-23 16:36:42 -0300

    add: reusable map component with marker files imported at /public for production deployment

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -3,5 +3,13 @@ import NotFound from "./NotFound";
 import TopBar from "./TopBar";
 import LogoCarousel from "./LogoCarousel";
 import TechnologiesCards from "./TechnologiesCards";
+import MapComponent from "./MapComponent";
 
-export { Footer, NotFound, TopBar, LogoCarousel, TechnologiesCards };
+export {
+  Footer,
+  NotFound,
+  TopBar,
+  LogoCarousel,
+  TechnologiesCards,
+  MapComponent,
+};
