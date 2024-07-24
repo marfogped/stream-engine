1266c99ba04b85e95da35224e03fffde80b8c056
BelenDeAmorrortu <belendeamorrortu@gmail.com>
2024-07-24 00:14:07 -0300

    fix: Video format to prevent issues with iOS devices & borders croped

--- src/constants/videos.ts
+++ src/constants/videos.ts
@@ -1,10 +1,10 @@
 import DefenseVideo from "../assets/videos/defense-video.mp4";
 import AdvertisingVideo from "../assets/videos/advertising-video.mp4";
 
-import SocialTechBackground from "../assets/videos/social-technology.webm";
-import TranslationsTechBackground from "../assets/videos/translations-technology.webm";
-import VisionTechBackground from "../assets/videos/vision-technology.webm";
-import VoiceTechBackground from "../assets/videos/voice-technology.webm";
+import SocialTechBackground from "../assets/videos/chat-technology.mp4";
+import TranslationsTechBackground from "../assets/videos/translation-technology.mp4";
+import VisionTechBackground from "../assets/videos/vision-technology.mp4";
+import VoiceTechBackground from "../assets/videos/voice-technology.mp4";
 
 export default {
   DefenseVideo,
