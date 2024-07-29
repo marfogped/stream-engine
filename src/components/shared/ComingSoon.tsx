a78a3c2aa4095b6297acadf3fff8ab8d0db171ac
Martin Fogliacco <martin@poreldeporte.com>
2024-07-29 18:52:50 -0300

    add: Coming Soon & Not Found pages

---
+++ src/components/shared/ComingSoon.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Typography } from '../ui'
+
+const ComingSoon:React.FC = () => {
+  return (
+    <div className="grid h-screen place-content-center bg-white px-4">
+        <Typography as="h1" variant="xl" className="uppercase tracking-widest text-gray-700">
+            Coming Soon
+        </Typography>
+        <Typography as="p" variant="md" className="tracking-widest text-gray-500">
+            We're working hard to bring you something amazing. Stay tuned!
+        </Typography>
+    </div>
+  )
+}
+
+export default ComingSoon
--- src/components/shared/NotFound.tsx
+++ src/components/shared/NotFound.tsx
@@ -1,7 +1,41 @@
+import { Typography } from "../ui";
+import { useNavigate } from "react-router-dom";
+import { HomeIcon, ArrowUturnLeftIcon } from "@heroicons/react/24/outline";
+import { Button } from "@chakra-ui/react";
+
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <div>NotFound</div>
+    <div className="h-screen mx-auto bg-white px-4 flex flex-col items-center justify-center">
+      <Typography as="h1" variant="md" className="uppercase tracking-widest text-gray-700">
+        404 | Not Found
+      </Typography>
+
+
+      <div className="flex justify-center w-full gap-2 pt-5">
+        <Button
+          onClick={() => navigate(-1)}
+          size={"lg"}
+          className="flex items-center gap-2"
+          bg="#F8F603"
+          _hover={{ bg: "#cebc00" }}
+        >
+          <ArrowUturnLeftIcon className="h-7 w-7" />
+          Go Back
+        </Button>
+
+        <Button
+          onClick={() => navigate('/')}
+          size={"lg"}
+          className="flex items-center gap-2"
+          bg="transparent"
+          _hover={{ bg: "#e5e5e5" }}
+        >
+          <HomeIcon className="h-7 w-7" />
+          Back to Home
+        </Button>
+      </div>
     </div>
   );
 };
