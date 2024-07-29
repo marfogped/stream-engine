6f54f7e9473b3d5de03c3c3e4325e6f2a41f4370
Martin Fogliacco <martin@poreldeporte.com>
2024-07-29 18:54:25 -0300

    feat: Homepage component lazy loaded

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
@@ -5,16 +6,23 @@ import "@fontsource-variable/dm-sans";
 import "./index.css";
 
 import RootLayout from "./layouts/root-layout";
-
-import Homepage from "./routes/homepage/Homepage";
 import PrivacyPolicy from "./routes/privacy-policy/PrivacyPolicy";
-import NotFound from "./components/shared/NotFound";
+import { NotFound, Loader } from "./components/shared";
+
+const Homepage = lazy(() => import("./routes/homepage/Homepage"));
 
 const router = createBrowserRouter([
   {
     element: <RootLayout />,
     children: [
-      { path: "/", element: <Homepage /> },
+      {
+        path: "/",
+        element: (
+          <Suspense fallback={<Loader />}>
+            <Homepage />
+          </Suspense>
+        ),
+      },
       { path: "/privacy-policy", element: <PrivacyPolicy /> },
     ],
     errorElement: <NotFound />,
