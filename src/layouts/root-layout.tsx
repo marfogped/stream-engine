c98e0ca051cb54c98a1499636a3e0caefcc80904
Martin Fogliacco <martin@poreldeporte.com>
2024-07-29 18:53:35 -0300

    feat: coming soon logic via .env file

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -1,8 +1,11 @@
 import { Outlet } from "react-router-dom";
-import TopBar from "../components/shared/TopBar";
-import Footer from "../components/shared/Footer";
+import {TopBar, Footer, ComingSoon} from "../components/shared";
+
+const displayComingSoon = import.meta.env.VITE_REACT_APP_DISPLAY_COMING_SOON === 'true';
 
 export default function RootLayout() {
+  if(displayComingSoon) return <ComingSoon />
+
   return (
     <>
       <TopBar />
