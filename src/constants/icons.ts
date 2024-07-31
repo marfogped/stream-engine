4d3c2aa1ffa5a9e8c3a257057e29e570f9e6fe84
Martin Fogliacco <martin@poreldeporte.com>
2024-07-31 19:26:26 -0300

    delete: unused icons

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -1,13 +1,9 @@
-import FacebookIcon from "../components/icons/facebook-icon";
 import InstagramIcon from "../components/icons/instagram-icon";
 import LinkedInIcon from "../components/icons/linkedin-icon";
-import WhatsAppIcon from "../components/icons/whatsapp-icon";
 import XIcon from "../components/icons/x-icon";
 
 export default {
-  FacebookIcon,
   InstagramIcon,
   LinkedInIcon,
-  WhatsAppIcon,
   XIcon,
 };
