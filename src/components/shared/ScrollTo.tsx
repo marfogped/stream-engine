eae5f68eb821a4e1866c0f36b53ad50a4ce07b1d
mrtfog <fogliaccomartin3@gmail.com>
2024-08-15 18:07:47 -0300

    fix: ScrollTo component logic to redirect to '/' path when user is in another path

--- src/components/shared/ScrollTo.tsx
+++ src/components/shared/ScrollTo.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Button } from "@chakra-ui/react";
+import { useLocation, useNavigate } from "react-router-dom";
 
 interface ScrollToProps {
   id: string;
   className?: string;
   size?: string;
   isChakraButton?: boolean;
+  isNavbarOpt?: boolean;
   children: React.ReactNode;
   onClick?: () => void;
 }
@@ -15,15 +17,21 @@ const ScrollTo: React.FC<ScrollToProps> = ({
   className,
   size,
   onClick,
+  isNavbarOpt = false,
   isChakraButton = false,
   children,
 }) => {
-  const scrollToSection = () => {
-    const section = document.getElementById(id);
-    if (section) {
-      section.scrollIntoView({ behavior: "smooth" });
-    }
+  const navigate = useNavigate();
+  const location = useLocation();
 
+  const scrollToSection = () => {
+    if (location.pathname !== "/" && isNavbarOpt) navigate("/");
+    setTimeout(() => {
+      const section = document.getElementById(id);
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+      }
+    }, 100);
     if (onClick) {
       onClick();
     }
